d4a0dac44b972fd4c88fff365f7dceffc3e8b58a
Nadia Shamsedin <nshamsedin@gmail.com>
2017-05-18 13:45:39 -0400

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ function getURLParameters(whichParam)
 {
 	var pageURL = window.location.search.substring(1);
 	var pageURLVariables = pageURL.split('&');
-	for(var i = 0; i < pageURLVariables.length[i]; i++){
+	for(var i = 0; i < pageURLVariables.length; i++){
 	    var parameterName = pageURLVariables[i].split('=');
 		if(parameterName[0] == whichParam){
 		return parameterName[1];
